dd0fd84e52a68e19cfeb0e94d4dab1e4f96ba0d1
Isahi.Rea <Isahi.rea@live.com>
2025-01-15 18:53:36 -0600

    Basic Crud Operations

--- src/main/java/com/IsahiRea/runnerz/RunnerzApplication.java
+++ src/main/java/com/IsahiRea/runnerz/RunnerzApplication.java
@@ -7,7 +7,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 import java.time.LocalDateTime;
