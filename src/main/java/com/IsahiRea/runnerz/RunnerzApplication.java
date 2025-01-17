253063f3d1595b77847ca699397291cc05ba155e
Isahi.Rea <Isahi.rea@live.com>
2025-01-16 18:03:58 -0600

    Added In-memory database

--- src/main/java/com/IsahiRea/runnerz/RunnerzApplication.java
+++ src/main/java/com/IsahiRea/runnerz/RunnerzApplication.java
@@ -2,6 +2,8 @@ package com.IsahiRea.runnerz;
 
 import com.IsahiRea.runnerz.run.Location;
 import com.IsahiRea.runnerz.run.Run;
+import com.IsahiRea.runnerz.run.RunRepository;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -21,11 +23,11 @@ public class RunnerzApplication {
 		SpringApplication.run(RunnerzApplication.class, args);
 	}
 
-	@Bean
-	CommandLineRunner runner() {
-		return args -> {
-			Run run = new Run(1,"First Run", LocalDateTime.now(), LocalDateTime.now().plus(1, ChronoUnit.HOURS), 5, Location.OUTDOOR);
-			log.info("Run: " + run);
-		};
-	}
+	// @Bean
+	// CommandLineRunner runner(RunRepository runRepository) {
+	// 	return args -> {
+	// 		Run run = new Run(1,"First Run", LocalDateTime.now(), LocalDateTime.now().plus(1, ChronoUnit.HOURS), 5, Location.OUTDOOR);
+	// 		runRepository.create(run);
+	// 	};
+	// }
 }
---
+++ src/main/java/com/IsahiRea/runnerz/run/Runs.java
@@ -0,0 +1,7 @@
+package com.IsahiRea.runnerz.run;
+
+import java.util.List;
+
+public record Runs(List<Run> runs) {
+    
+}
