5b2b6a284ca39f350af32ac9a2518e66916168ec
Isahi.Rea <Isahi.rea@live.com>
2025-01-20 22:00:19 -0600

    Created User Rest Client

---
+++ src/main/java/com/IsahiRea/runnerz/user/UserRestClient.java
@@ -0,0 +1,16 @@
+package com.IsahiRea.runnerz.user;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+@Component
+public class UserRestClient {
+    
+    private final RestClient restClient;
+
+    public UserRestClient(RestClient.Builder builder) {
+        this.restClient = builder
+        .baseUrl("https://jsonplaceholder.typicode.com")
+        .build();
+    }
+}
